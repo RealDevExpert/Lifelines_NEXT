5d1f07c41fa94640f712caafa6c8e3df6b51523a
RealDevExpert <georgedotjmartinez@outlook.com>
2023-05-05 21:04:29 +0200

    Update 21.Composition_difference_by_Type_Bray.R
    Added NMDS based on RPKM counts of viruses aggregated by host

--- NEXT_pilot_microbial_transmission_mother_infant/Downstream_analysis/21.Composition_difference_by_Type_Bray.R
+++ NEXT_pilot_microbial_transmission_mother_infant/Downstream_analysis/21.Composition_difference_by_Type_Bray.R
@@ -31,6 +31,8 @@ VLP_metadata <- read.table('02.CLEAN_DATA/VLP_metadata_with_phenos.txt', sep='\t
 
 RPKM_counts_VLP <- read.table('02.CLEAN_DATA/RPKM_counts_VLP.txt', sep='\t', header = T)
 VLP_metadata$Type <- factor(VLP_metadata$Type, levels=c('Infant', 'Mother'), ordered=T)
+
+RPKM_virome_aggr <- read.table('02.CLEAN_DATA/RPKM_counts_aggregated_bacterial_host_raw.txt', sep='\t', header=T)
 ##############################
 # ANALYSIS
 ##############################
@@ -94,7 +96,7 @@ if (identical(colnames(microbiome), MGS_metadata$Short_sample_ID_bact)) {
                                 by = "margin")
 }
 
-#### NMDS for bacteriome
+#### NMDS for virome
 
 # preapring phenotypes
 for_virplot <- VLP_metadata
@@ -157,8 +159,71 @@ if (identical(colnames(RPKM_counts_VLP), VLP_metadata$Short_sample_ID)) {
                                 by = "margin")
 }
 
+#### NMDS for viruses aggregated based on bacterial hosts at the species-level
+
+# preapring phenotypes
+for_viragrplot <- VLP_metadata
+row.names(for_viragrplot) <- VLP_metadata$Short_sample_ID
+for_viragrplot <- for_viragrplot[colnames(RPKM_virome_aggr),]
+#CHOOSING TECHNICAL PHENOTYPES, SAMPLE TYPE & VIRAL ALPHA DIVERSITY
+for_viragrplot<- for_viragrplot[,c("Type", "Clean_reads", "DNA_CONC", "viral_alpha_diversity", "bacterial_contamination_perc_reads")]
+
+#### yes, I use the same object names and don't make a function out of it
+ord <- metaMDS(t(RPKM_virome_aggr), distance = "bray", k=2)
+en = envfit(ord, for_viragrplot, permutations = 999, na.rm = TRUE)
+en$factors #r2 0.37; p-value < 0.001
+en$vectors
+
+centroids <- as.data.frame(scores(en, "factors"))
+centroids$Type <- c(gsub('Type', '', row.names(centroids)))
+
+spp.scrs <- as.data.frame(scores(en, display = "vectors"))
+spp.scrs <- cbind(spp.scrs, Species = rownames(spp.scrs))
+spp.scrs$Type <- 'Infant'
+spp.scrs <- spp.scrs[spp.scrs$Species!='Clean_reads',] # because it was not significant in envfit
+spp.scrs$Species <- c('DNA concentration', 'Alpha diversity (viruses)', '% bacterial contamination')
+
+### for plotting:
+data.scores = as.data.frame(scores(ord, "sites"))
+data.scores <- merge(data.scores, for_viragrplot, by='row.names')
+row.names(data.scores) <- data.scores$Row.names
+data.scores$Row.names <- NULL
+
+pdf('./04.PLOTS/Viruses_aggregated_Bray_NMDS.pdf', width=10/2.54, height=10/2.54)
+gg = ggplot(data = data.scores, aes(x = NMDS1, y = NMDS2, color=Type)) + 
+  geom_point(size = 2, alpha=0.8) + 
+  geom_point(data=centroids, aes(fill=Type),shape=23, size=4, color='black', ) + 
+  stat_ellipse(geom = "polygon", alpha = 0.0, aes(group = Type, color=Type), linetype = 2)+
+  geom_segment(data = spp.scrs,
+               aes(x = 0, xend = NMDS1, y = 0, yend = NMDS2),
+               arrow = arrow(length = unit(0.25, "cm")), colour = "grey") +
+  geom_label_repel(data = spp.scrs, aes(x = NMDS1, y = NMDS2, label = Species), color='black', alpha = 0.8, size = 2) +
+  theme_bw()+
+  theme(axis.text=element_text(size=10),
+        axis.title = element_text(size = 12, face = "bold", colour = "grey30"), 
+        panel.background = element_blank(), 
+        panel.border = element_rect(fill = NA, colour = "grey30"), 
+        legend.title = element_text(size = 10, face = "bold", colour = "grey30"), 
+        legend.text = element_text(size = 12, colour = "grey30"),
+        legend.position = "bottom") + 
+  scale_color_manual(values=c("#F8766D", "#00BFC4"))
+
+ggMarginal(gg, type="densigram", groupFill=T)
+dev.off()
+
+
+### calculating the significance of separation:
+RPKM_virome_aggr <- RPKM_virome_aggr[,VLP_metadata$Short_sample_ID]
+if (identical(colnames(RPKM_virome_aggr), VLP_metadata$Short_sample_ID)) {
+  viruses_aggr_permanova <- adonis2(t(RPKM_virome_aggr) ~ Type +viral_alpha_diversity + Clean_reads + DNA_CONC + bacterial_contamination_perc_reads, 
+                               data=VLP_metadata,  
+                               permutations = 999,
+                               method="bray",
+                               by = "margin")
+}
 
 
 ###### OUTPUT #####
 write.table(bacteria_permanova, '03a.RESULTS/Bacterial_spp_bray_PERMANOVA.txt', sep='\t', quote=F)
 write.table(viruses_permanova, '03a.RESULTS/Viral_vOTUs_bray_PERMANOVA.txt', sep='\t', quote=F)
+write.table(viruses_aggr_permanova, '03a.RESULTS/Viruses_aggregated_bray_PERMANOVA.txt', sep='\t', quote=F)
