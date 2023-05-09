8d5716c64f6d4bf4a6613b0f510ca122779a5960
RealDevExpert <georgedotjmartinez@outlook.com>
2023-05-09 16:18:10 +0200

    Update 09.Bacterial_viral_alpha_diversity_tests.R
    Minor updates

--- NEXT_pilot_microbial_transmission_mother_infant/Downstream_analysis/09.Bacterial_viral_alpha_diversity_tests.R
+++ NEXT_pilot_microbial_transmission_mother_infant/Downstream_analysis/09.Bacterial_viral_alpha_diversity_tests.R
@@ -20,6 +20,7 @@ library(lmerTest)
 library(reshape2)
 library(ggplot2)
 library(ggforce)
+library(ggsignif)
 ##############################
 # Input data
 ##############################
@@ -37,7 +38,6 @@ MGS_metadata$Timepoint <- factor(MGS_metadata$Timepoint, levels = c("P3", "P7", 
                                                                     "M6", "M9", "M12"), ordered = T)
 MGS_metadata$Type <- as.factor(MGS_metadata$Type)
 MGS_metadata$Short_sample_ID <- row.names(MGS_metadata)
-
 
 # alpha-diversity in virome
 
@@ -93,9 +93,22 @@ ggplot(VLP_metadata, aes(Timepoint, viral_alpha_diversity, fill=Type)) +
   labs(colour = "Type")
 dev.off()
 
+# similar to maternal virome diversity at the later timepoints?
+wilcox.test(VLP_metadata[VLP_metadata$Timepoint=='M12',]$viral_alpha_diversity, VLP_metadata[VLP_metadata$Type=='Mother',]$viral_alpha_diversity)
+
+n_m <- length(VLP_metadata[VLP_metadata$Type=='Mother',]$viral_alpha_diversity)
+n_i <- length(VLP_metadata[VLP_metadata$Timepoint=='M12',]$viral_alpha_diversity)
+s_m <- sd(VLP_metadata[VLP_metadata$Type=='Mother',]$viral_alpha_diversity)
+s_i <- sd(VLP_metadata[VLP_metadata$Timepoint=='M12',]$viral_alpha_diversity)
+mean_m <- mean(VLP_metadata[VLP_metadata$Type=='Mother',]$viral_alpha_diversity)
+mean_i <- mean(VLP_metadata[VLP_metadata$Timepoint=='M12',]$viral_alpha_diversity)
+s_p = sqrt(((n_m - 1) * s_m^2 + (n_i - 1) * s_i^2) / (n_m + n_i - 2))
+Cohens_d = (mean_m - mean_i) / s_p
+
 # dependent on phenotypes in infants? 
 btmod1_feeding  = lmer(viral_alpha_diversity ~ Age_days + infant_ever_never_breastfed + DNA_CONC + Clean_reads + (1|NEXT_ID), REML = F, data = VLP_metadata[VLP_metadata$Type=="Infant",])
 summary(btmod1_feeding) #infant_ever_never_breastfed  8.786e-01  3.422e-01  1.941e+01   2.567   0.0187 *
+
 
 pdf('./04.PLOTS/Virome_diversity_feeding_plot.pdf', width=12/2.54, height=9/2.54)
 
@@ -124,7 +137,7 @@ type_mod0_bac <- lm(bacterial_alpha_diversity ~ Type + DNA_CONC + Clean_reads, d
 type_mod1_bac  = lmer(bacterial_alpha_diversity ~ Type + DNA_CONC + Clean_reads + (1|NEXT_ID), REML = F, data = MGS_metadata)
 BIC(type_mod0_bac, type_mod1_bac)
 exactLRT(type_mod1_bac,type_mod0_bac)
-summary(type_mod1_bac) # Type 1.870083  0.1043775 75.68553 17.91653 6.028737e-29
+summary(type_mod1_bac) # Type 1.767321e+00 9.933489e-02  76.76975 17.791543 5.861632e-29
 as.data.frame(summary(type_mod1_bac)$coefficients)[,1:5]
 
 
@@ -133,7 +146,7 @@ mod0_bac <- lm(bacterial_alpha_diversity ~ Timepoint_continuous + DNA_CONC + Cle
 mod1_bac  = lmer(bacterial_alpha_diversity ~ Type + Timepoint_continuous + DNA_CONC + Clean_reads + (1|NEXT_ID), REML = F, data = MGS_metadata)
 BIC(mod0_bac, mod1_bac)
 exactLRT(mod1_bac,mod0_bac)
-summary(mod1_bac) #Timepoint_continuous 5.879e-02  7.145e-03  2.748e+02   8.228  7.7e-15 *** 
+summary(mod1_bac) #Timepoint_continuous 5.719e-02  7.041e-03  2.759e+02   8.122 1.54e-14 ***
 
 
 
@@ -142,7 +155,7 @@ btmod0_bac <- lm(bacterial_alpha_diversity ~ Age_days + DNA_CONC + Clean_reads, 
 btmod1_bac  = lmer(bacterial_alpha_diversity ~ Age_days + DNA_CONC + Clean_reads + (1|NEXT_ID), REML = F, data = MGS_metadata[MGS_metadata$Type=="Infant",])
 BIC(btmod0_bac, btmod1_bac)
 exactLRT(btmod1_bac,btmod0_bac)
-summary(btmod1_bac) #Age_days     2.727e-03  3.524e-04  1.552e+02   7.738  1.2e-12 ***
+summary(btmod1_bac) #Age_days     2.707e-03  3.521e-04  1.552e+02   7.689 1.59e-12 ***
 
 
 # dependant on time in mothers? NS
@@ -150,7 +163,7 @@ mtmod0_bac <- lm(bacterial_alpha_diversity ~ Timepoint_continuous+ DNA_CONC + Cl
 mtmod1_bac  = lmer(bacterial_alpha_diversity ~ Timepoint_continuous+ DNA_CONC + Clean_reads + (1|NEXT_ID), REML = F, data = MGS_metadata[MGS_metadata$Type=="Mother",])
 BIC(mtmod0_bac, mtmod1_bac)
 exactLRT(mtmod1_bac,mtmod0_bac)
-summary(mtmod1_bac) #Timepoint_continuous 4.017e-04  6.795e-02 9.027e+01   0.006   0.9953   
+summary(mtmod1_bac) #Timepoint_continuous 1.125e-02  1.001e-02  1.204e+02   1.124   0.2634   
 
 
 # alpha-diversity plot bacteriome
@@ -197,4 +210,30 @@ ggplot(bacteriome_feeding_plot, aes(Timepoint, value, fill=infant_ever_never_bre
                     values=c("#FAE3D9", "#BBDED6"))
 dev.off()
 
+# plot for virome and bacteriome together:
+together_plot <- rbind(data.frame(unname(VLP_metadata[,c("Type", "viral_alpha_diversity")])),
+                       data.frame(unname(MGS_metadata[,c("Type", "bacterial_alpha_diversity")])))
+together_plot$Source <- substr(row.names(together_plot), 8,8)
+together_plot[together_plot$Source=='V',]$Source <- 'Virome'
+together_plot[together_plot$Source=='M',]$Source <- 'Bacteriome'
+together_plot$Source <- factor(together_plot$Source, levels = c('Virome', 'Bacteriome'), ordered = T)
+together_plot$significance_level <- '***'
+
+pdf('./04.PLOTS/Virome_bacteriome_diversity_together.pdf', width=12/2.54, height=12/2.54)
+
+ggplot(together_plot, aes(X1, X2, fill=X1)) + 
+  labs (y="Shannon Diversity Index", x="") + 
+  geom_violin(alpha=0.7) + 
+  geom_boxplot(width = 0.1, outlier.shape = NA, alpha=0.9) + 
+  geom_sina(colour='#303841', size=0.5) +
+  theme_bw()+
+  theme(axis.text=element_text(size=12), 
+        axis.title=element_text(size=16,face="bold"),
+        strip.text.x = element_text(size = 12),
+        legend.position = "none",
+        legend.title = element_text(size=12, face="bold")) +
+  geom_signif(comparisons = list(c("Infant", "Mother")), 
+              map_signif_level=TRUE) +
+  facet_grid(~Source)
+dev.off()
 
