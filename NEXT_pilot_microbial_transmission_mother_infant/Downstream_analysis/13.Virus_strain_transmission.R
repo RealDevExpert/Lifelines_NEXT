15bc9c80cee735d7822999e355a2a83ce33e36c2
RealDevExpert <georgedotjmartinez@outlook.com>
2023-06-01 21:01:49 +0200

    Update 13.Virus_strain_transmission.R
    Removed packages and data that is not used in the script, modified description and plots.

--- NEXT_pilot_microbial_transmission_mother_infant/Downstream_analysis/13.Virus_strain_transmission.R
+++ NEXT_pilot_microbial_transmission_mother_infant/Downstream_analysis/13.Virus_strain_transmission.R
@@ -1,8 +1,9 @@
 setwd('~/Desktop/Projects_2022/NEXT_pilot_FUP/')
 
 #############################################################
-# Here we explore how virus 'strains' are shared between
-# related and unrelated individuals
+# Here we compare genetic pairwise distances of virus
+# consensuses reconstructed from VLP and MGS samples between
+# related and unrelated mother-infant pairs
 #############################################################
 
 ##############################
@@ -14,25 +15,26 @@ setwd('~/Desktop/Projects_2022/NEXT_pilot_FUP/')
 ##############################
 library(ggplot2)
 library(ggforce)
-
 library(ggforestplot)
+library(reshape2)
+library(patchwork)
 
-library(scales)
+#library(scales)
 ##############################
 # Input data
 ##############################
-metadata <- read.table('03.SCRIPTS/NEXT_pilot_FUP_bf_origin/Data_for_Alex/metadata_combined_for_exp.txt', sep='\t', header=T)
-
-RPKM_combined_0.95 <- read.table('03.SCRIPTS/NEXT_pilot_FUP_bf_origin/Data_for_Alex/RPKM_counts_combined_0.95_UPD_final_for_exp.txt', sep='\t', header=T)
 
 selected_viruses <- read.table('03.SCRIPTS/NEXT_pilot_FUP_bf_origin/Data_for_Alex/Viruses_shared_min_5_families_UPD_final.txt', sep='\t', header=T)
-
+# remove columns that are not informative for the further analysis:
+exlude_columns <- c("excluding", "final_exclusion", "Order$", "Percent_of_votes", "Family$", "Percent_of_votes.1", "geNomad_taxonomy$")
+selected_viruses <- selected_viruses[,grep(paste(exlude_columns,collapse="|"), 
+                                          colnames(selected_viruses), invert = T)]
+colnames(selected_viruses)[1] <- 'Virus'
 
 folder = "02.CLEAN_DATA/dist.matrices/"  
 file_list = list.files(path=folder, pattern="*.txt")  
 virus=lapply(paste0(folder, file_list), function(x) read.table(x, sep='\t', header=T))
 names(virus) <- gsub(".dist.txt", '', file_list)
-
 
 ##############################
 # ANALYSIS
@@ -70,7 +72,7 @@ for (n in 1:NROW(virus) ) {
       # Assigning all pair-wise distances between samples of related individuals as NA for the selected family
       
       mother_unrelated_distances[[i]] <- unlist( virusN[grep( paste0(i, '_Mother') , row.names(virusN) ), 
-                                                        grep( paste0(i, '_Infant'), colnames( virusN ), invert = T ) ] )
+                                                        grep( paste0(i, '_Infant'), colnames(virusN), invert = T ) ] )
       
     } else {
       
@@ -96,24 +98,35 @@ colnames(p_value_real)[c(1:3)] <- c("Virus", "N_related_distances", "p_value")
 plot_distances <- data.frame()
 
 for (n in 1:NROW(virus)) {
-  p_value_real[n,1] <- names(virus[n])
-  p_value_real[n,2] <- length(mother_infant_distances_virus[[n]])
+  p_value_real[n,"Virus"] <- names(virus[n])
+  p_value_real[n,"N_related_distances"] <- length(mother_infant_distances_virus[[n]])
+  
   if (length(unrelated_distances_virus[[n]])!=0) {
+    
     vector4analysis = c(mother_infant_distances_virus[[n]], unrelated_distances_virus[[n]])
     factor4analysis = c(rep("Related",length(mother_infant_distances_virus[[n]])),
                         rep("Unrelated",length(unrelated_distances_virus[[n]])))
+    
     wilcoxon_real = wilcox.test(vector4analysis ~ factor4analysis, alternative='less', paired=F)
-    p_value_real[n,3] <- wilcoxon_real$p.value
+    p_value_real[n,"p_value"] <- wilcoxon_real$p.value
     
     #table for plot
     virus_name <- c( rep( names(virus[n]), length(factor4analysis) ) )
     plot_distances <- rbind(plot_distances, data.frame(virus_name, factor4analysis, vector4analysis))
+    
   } else {
+    # if there were no distances to unrelated infants for the mother:
     p_value_real[n,3] <- "Comparison not possible"
-  }
+  
+    }
   
 }
 
+selected_viruses <- merge(selected_viruses, p_value_real[,c("Virus", "N_related_distances","p_value")], by='Virus', all.x = T)
+colnames(selected_viruses)[length(selected_viruses)] <- 'Related_unrelated_comparison_possible'
+selected_viruses$Related_unrelated_comparison_possible <- ifelse(selected_viruses$Related_unrelated_comparison_possible=='Comparison not possible',
+                                                                 "NO",
+                                                                 "YES")
 
 #permutations test
 
@@ -209,15 +222,27 @@ for (i in p_value_real$Virus) {
 family_viruses <- p_value_real[!is.na(p_value_real$p_value_adj),]
 family_viruses$FDR <- p.adjust(family_viruses$p_value_adj, method = "BH")
 
+selected_viruses <- merge(selected_viruses, family_viruses[,c("Virus","FDR")], by='Virus', all.x = T)
+colnames(selected_viruses)[length(colnames(selected_viruses))] <- "FDR_dist_comparison"
+selected_viruses$Distances_Related_lower <- ifelse(selected_viruses$FDR_dist_comparison<=0.05, "YES", "NO")
+
+if (identical(selected_viruses$Virus, names(unrelated_distances_virus))) {
+  
+  selected_viruses$N_unrelated_distances <- as.numeric(lapply(unrelated_distances_virus, function(x) {
+    length(x)
+  }))
+  
+}
+
 
 ##### PLOTS ####
 # adding the smallest non-zero Kimura distance to all distances (to use logarithmic scale in the plot)
 plot_distances$vector4analysis <- plot_distances$vector4analysis + min(plot_distances[plot_distances$vector4analysis!=0,]$vector4analysis)
 # showing only those viruses that have more than 5 pair-wise distances for related samples
 plot_distances_select <- plot_distances[ plot_distances$virus_name %in% names(virus)[lengths(mother_infant_distances_virus)>5] ,]
+
 # renaming contigs for easier perception:
-plot_distances_select$easy_name <- paste0( 'L_', gsub('.*_length_','',plot_distances_select$virus_name))
-plot_distances_select$easy_name <- gsub("\\...\\K\\d+", "", plot_distances_select$easy_name, perl = TRUE)
+plot_distances_select$easy_name <- selected_viruses$ContigID_easy[match(plot_distances_select$virus_name, selected_viruses$Virus)]
 
 # color-coding the y-axis titles depending on statistical significance of the differnece:
 myPalette <- family_viruses
@@ -225,58 +250,119 @@ myPalette$color <- NA
 myPalette[myPalette$FDR>0.05,]$color <- 'grey'
 myPalette[myPalette$FDR<=0.05,]$color <- 'black'
 myPalette <- myPalette[myPalette$Virus %in% unique(plot_distances_select$virus_name),]
-myPalette$easy_name <-  paste0( 'L_', gsub('.*_length_','',myPalette$Virus))
+myPalette$easy_name <-  selected_viruses$ContigID_easy[match(myPalette$Virus, selected_viruses$Virus)]
 myPalette <- myPalette[order(myPalette$easy_name),]
 
 # all virus strains
-pdf('./04.PLOTS/Infant_virus_strain_all_wilcox_less.pdf', width=29.7/2.54, height=21/2.54)
-ggplot(plot_distances_select, aes(vector4analysis,easy_name, fill=factor4analysis)) + 
-  geom_boxplot(outlier.shape = NA,alpha=0.5) +
-  labs (y="Viruses", x="Log-scaled Kimura distance") + 
-  geom_sina(aes(fill=factor4analysis), size=0.6,alpha=0.5) +
-  #geom_jitter(,size=0.6,alpha=0.5) +
+p1 <- ggplot(plot_distances_select, aes(vector4analysis,easy_name, fill=factor4analysis)) + 
+  labs (y="Viruses", x="Log-scaled\nKimura distance") + 
+  geom_sina(aes(fill=factor4analysis), size=1.3, alpha=0.8, shape=21, stroke=0) +
+  geom_boxplot(outlier.shape = NA, alpha=0.3) +
   theme_bw()+
   scale_x_log10() +
-  theme(axis.text=element_text(size=12), 
-        axis.title=element_text(size=12,face="bold"),
+  theme(axis.text=element_text(size=8), 
+        axis.title=element_text(size=8,face="bold"),
         strip.text.x = element_text(size = 12),
         legend.text = element_text(size=10),
-        legend.title = element_text(size=12, face="bold")) +
-  labs(fill="Kinship") + geom_stripes(odd = "#33333333", even = "#00000000") +
+        legend.title = element_text(size=12, face="bold"),
+        plot.margin = margin(0, 0, 0, 0, "cm")) +
+  labs(fill="Kinship", color="") + geom_stripes(odd = "#33333333", even = "#00000000") +
   scale_fill_manual(labels = c("Related", "Unrelated"), 
                     values=c("#17B971", "#7d8bb1")) + 
-  theme(axis.text.y = element_text(colour=myPalette$color) )
-dev.off()
+  theme(axis.text.y = element_text(colour=myPalette$color),
+        legend.position = "none")
 
+
+N_pairwise_distance <- melt(selected_viruses[,c("Virus", "ContigID_easy", "N_related_distances", "N_unrelated_distances")])
+N_pairwise_distance$value <- N_pairwise_distance$value+1
+
+
+p2 <- ggplot(N_pairwise_distance, aes(value, ContigID_easy, fill=variable) ) + 
+  geom_bar(stat='identity', position='dodge', color="black", alpha=0.5) +
+  scale_x_log10(breaks=c(10^0, 10^1, 10^3)) + 
+  labs (y="", x="Log-scaled\nN distances") +
+  theme_bw()+
+  theme(axis.text=element_text(size=8), 
+        axis.title=element_text(size=8,face="bold"),
+        strip.text.x = element_text(size = 12),
+        legend.text = element_text(size=10),
+        legend.title = element_text(size=12, face="bold"),
+        axis.text.y = element_blank(),
+        axis.ticks.y = element_blank(),
+        legend.position = "none",
+        plot.margin = margin(0, 0, 0, 0, "cm")) +
+  labs(fill="Kinship")+
+  geom_stripes(odd = "#33333333", even = "#00000000") +
+  scale_fill_manual(labels = c("Related", "Unrelated"), 
+                    values=c("#17B971", "#7d8bb1")) +
+  guides(fill = "none")
+
+
+combined_plot <- p1 + p2 +
+  plot_layout(ncol = 2, nrow = 1, guides="collect", widths = c(4, 1.5)) + 
+  plot_annotation(title = "") & theme(legend.position = 'bottom') 
+
+pdf('./04.PLOTS/Infant_virus_strain_all_more_than_7_fams_wilcox_less_final.pdf', width=9/2.54, height=17/2.54)
+combined_plot
+dev.off()
 
 # those where distances are significantly different:
 
-myPalette <- myPalette[myPalette$FDR<0.05,]
+myPalette$N_positive_families <- selected_viruses$N_positive_families[match(myPalette$Virus, selected_viruses$Virus)]
+myPalette <- myPalette[myPalette$FDR<0.05 & myPalette$N_positive_families >=7,]
 plot_distances_select <- plot_distances_select[plot_distances_select$virus_name %in% myPalette$Virus,]
 
+N_pairwise_distance <- N_pairwise_distance[N_pairwise_distance$Virus %in% myPalette$Virus,]
 
-pdf('./04.PLOTS/Infant_virus_strains_significant_wilcox_less.pdf', width=29.7/2.54, height=21/2.54)
-ggplot(plot_distances_select, aes(vector4analysis, easy_name, fill=factor4analysis)) + 
-  geom_boxplot(outlier.shape = NA,alpha=0.5) +
-  labs (y="Viruses", x="Log-scaled Kimura distance") + 
-  geom_sina(aes(fill=factor4analysis), size=0.6,alpha=0.5) +
-  #geom_jitter(aes(),size=0.6,alpha=0.5) +
+p3 <- ggplot(plot_distances_select, aes(vector4analysis, easy_name, fill=factor4analysis)) + 
+  labs (y="Viruses", x="Log-scaled\nKimura distance") + 
+  geom_sina(aes(fill=factor4analysis), size=0.9, alpha=0.8, shape=21, stroke=0) +
+  geom_boxplot(outlier.shape = NA, alpha=0.3) +
   theme_bw()+
   scale_x_log10() +
-  theme(axis.text=element_text(size=12), 
-        axis.title=element_text(size=12,face="bold"),
+  theme(axis.text=element_text(size=6), 
+        axis.title=element_text(size=6,face="bold"),
         strip.text.x = element_text(size = 12),
-        legend.text = element_text(size=10),
-        legend.title = element_text(size=12, face="bold")) +
-  labs(fill="Kinship") + geom_stripes(odd = "#33333333", even = "#00000000") +
+        legend.text = element_text(size=6),
+        legend.title = element_text(size=8, face="bold"),
+        plot.margin = margin(0, 0, 0, 0, "cm")) +
+  labs(fill="Kinship", color="") + geom_stripes(odd = "#33333333", even = "#00000000") +
   scale_fill_manual(labels = c("Related", "Unrelated"), 
                     values=c("#17B971", "#7d8bb1")) + 
-  theme(axis.text.y = element_text(colour=myPalette$color) )
-dev.off()
+  theme(axis.text.y = element_text(colour=myPalette$color),
+        legend.position = "none")
 
+p4 <- ggplot(N_pairwise_distance, aes(value, ContigID_easy, fill=variable) ) + 
+  geom_bar(stat='identity', position='dodge', color="black", alpha=0.5) +
+  scale_x_log10(breaks=c(10^0, 10^1, 10^3)) + 
+  labs (y="", x="Log-scaled\nN distances") +
+  theme_bw()+
+  theme(axis.text=element_text(size=6), 
+        axis.title=element_text(size=6,face="bold"),
+        strip.text.x = element_text(size = 12),
+        legend.text = element_text(size=6),
+        legend.title = element_text(size=8, face="bold"),
+        axis.text.y = element_blank(),
+        axis.ticks.y = element_blank(),
+        legend.position = "none",
+        plot.margin = margin(0, 0, 0, 0, "cm")) +
+  labs(fill="Kinship")+
+  geom_stripes(odd = "#33333333", even = "#00000000") +
+  scale_fill_manual(labels = c("Related", "Unrelated"), 
+                    values=c("#17B971", "#7d8bb1")) +
+  guides(fill = "none")
+
+combined_plot_significant <- p3 + p4 +
+  plot_layout(ncol = 2, nrow = 1, guides="collect", widths = c(5, 1.5)) + 
+  plot_annotation(title = "") & theme(legend.position = 'bottom') 
+
+pdf('./04.PLOTS/Infant_virus_strains_significant_more_than_7_fams_wilcox_less_final.pdf', width=7.7/2.54, height=14/2.54)
+combined_plot_significant
+dev.off()
 ##############################
 # OUTPUT
 ##############################
-
+write.table(plot_distances, '02.CLEAN_DATA/PREPARED_DATA_FOR_PLOTS/Virus_distances_related_vs_unrelated.txt', sep='\t', quote=F, row.names=F)
+write.table(selected_viruses, '02.CLEAN_DATA/List_viruses_selected_transmission_metadata.txt', sep='\t', quote=F, row.names=F)
 write.table(family_viruses, '02.CLEAN_DATA/List_viruses_results_checking_transmission.txt', sep='\t', quote=F, row.names = F)
 
