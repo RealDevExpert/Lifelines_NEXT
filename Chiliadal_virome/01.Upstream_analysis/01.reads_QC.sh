2ac7271f05b38e620d3685e9e083b83ae29c99eb
RealDevExpert <georgedotjmartinez@outlook.com>
2023-08-07 20:21:59 +0200

    Update 01.reads_QC.sh
    Added step of adapter and quality trimming.
    !!! Did not use minlen parameter

--- Chiliadal_virome/01.Upstream_analysis/01.reads_QC.sh
+++ Chiliadal_virome/01.Upstream_analysis/01.reads_QC.sh
@@ -15,23 +15,38 @@ module purge
 module load BBMap
 
 #### TRIMMING ADAPTASE-INTRODUCED TAILS 
+#bbduk.sh \
+#	in=../MVS_raw_reads/${SAMPLE_ID}/raw_reads/${SAMPLE_ID}_1.fastq.gz \
+#	out=../MVS_raw_reads/${SAMPLE_ID}/raw_reads/${SAMPLE_ID}_1.NoTail.fastq.gz \
+#	ftr2=11 tpe tbo 2>&1 \
+#        threads=${SLURM_CPUS_PER_TASK} \
+#	-Xmx32g | tee ../MVS_raw_reads/${SAMPLE_ID}/raw_reads/${SAMPLE_ID}_bbduk.log
+#bbduk.sh \
+#        in=../MVS_raw_reads/${SAMPLE_ID}/raw_reads/${SAMPLE_ID}_2.fastq.gz \
+#        out=../MVS_raw_reads/${SAMPLE_ID}/raw_reads/${SAMPLE_ID}_2.NoTail.fastq.gz \
+#        ftl=11 tpe tbo 2>&1 \
+#        threads=${SLURM_CPUS_PER_TASK} \
+#        -Xmx32g | tee ../MVS_raw_reads/${SAMPLE_ID}/raw_reads/${SAMPLE_ID}_bbduk.log
+
+#### TRIMMING ADAPTERS & LOW-QUALITY READS
 bbduk.sh \
-	in=../MVS_raw_reads/${SAMPLE_ID}/raw_reads/${SAMPLE_ID}_1.fastq.gz \
-	out=../MVS_raw_reads/${SAMPLE_ID}/raw_reads/${SAMPLE_ID}_1.NoTail.fastq.gz \
-	ftr2=11 tpe tbo 2>&1 \
-        threads=${SLURM_CPUS_PER_TASK} \
+	in1=../MVS_raw_reads/${SAMPLE_ID}/raw_reads/${SAMPLE_ID}_1.NoTail.fastq.gz \
+	in2=../MVS_raw_reads/${SAMPLE_ID}/raw_reads/${SAMPLE_ID}_2.NoTail.fastq.gz \
+	out1=../MVS_raw_reads/${SAMPLE_ID}/raw_reads/${SAMPLE_ID}_AdaptTr_QualTr_1.fastq.gz \
+    	out2=../MVS_raw_reads/${SAMPLE_ID}/raw_reads/${SAMPLE_ID}_AdaptTr_QualTr_2.fastq.gz \
+	ref=/scratch/p282752/Data_for_HiC/adapters_UPD_IDT.fa \
+	ktrim=r k=23 mink=11 hdist=1 tpe tbo \
+	qtrim=rl trimq=20 \
+	threads=${SLURM_CPUS_PER_TASK} \
 	-Xmx32g | tee ../MVS_raw_reads/${SAMPLE_ID}/raw_reads/${SAMPLE_ID}_bbduk.log
-bbduk.sh \
-        in=../MVS_raw_reads/${SAMPLE_ID}/raw_reads/${SAMPLE_ID}_2.fastq.gz \
-        out=../MVS_raw_reads/${SAMPLE_ID}/raw_reads/${SAMPLE_ID}_2.NoTail.fastq.gz \
-        ftl=11 tpe tbo 2>&1 \
-        threads=${SLURM_CPUS_PER_TASK} \
-        -Xmx32g | tee ../MVS_raw_reads/${SAMPLE_ID}/raw_reads/${SAMPLE_ID}_bbduk.log
 
 module load FastQC
-fastqc ../MVS_raw_reads/${SAMPLE_ID}/raw_reads/${SAMPLE_ID}_1.NoTail.fastq.gz
-fastqc ../MVS_raw_reads/${SAMPLE_ID}/raw_reads/${SAMPLE_ID}_2.NoTail.fastq.gz
-mv ../MVS_raw_reads/${SAMPLE_ID}/raw_reads/${SAMPLE_ID}_*fastqc* ../MVS_raw_reads/FastQC_reports/NoTail/
+#fastqc ../MVS_raw_reads/${SAMPLE_ID}/raw_reads/${SAMPLE_ID}_1.NoTail.fastq.gz
+#fastqc ../MVS_raw_reads/${SAMPLE_ID}/raw_reads/${SAMPLE_ID}_2.NoTail.fastq.gz
+#mv ../MVS_raw_reads/${SAMPLE_ID}/raw_reads/${SAMPLE_ID}_*fastqc* ../MVS_raw_reads/FastQC_reports/NoTail/
+fastqc ../MVS_raw_reads/${SAMPLE_ID}/raw_reads/${SAMPLE_ID}_AdaptTr_QualTr_1.fastq.gz
+fastqc ../MVS_raw_reads/${SAMPLE_ID}/raw_reads/${SAMPLE_ID}_AdaptTr_QualTr_2.fastq.gz
+mv ../MVS_raw_reads/${SAMPLE_ID}/raw_reads/${SAMPLE_ID}_*AdaptTr_QualTr*fastqc* ../MVS_raw_reads/FastQC_reports/AdaptTr_QualTr/
 
 module list
 
