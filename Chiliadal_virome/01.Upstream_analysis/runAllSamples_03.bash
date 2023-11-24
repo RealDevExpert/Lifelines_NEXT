cec3077e9ef9ecfeb1539fda473500aeebfdd447
RealDevExpert <georgedotjmartinez@outlook.com>
2023-11-24 12:38:13 +0100

    Update runAllSamples_03.bash
    Changed the location of .out and .err

--- Chiliadal_virome/01.Upstream_analysis/runAllSamples_03.bash
+++ Chiliadal_virome/01.Upstream_analysis/runAllSamples_03.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 for SAMPLE in $@; do
- 	sbatch --output ./out/${SAMPLE}_03.out --error ./err/${SAMPLE}_03.err --job-name vQC_${SAMPLE} 03.virus_enrichment_index.sh ${SAMPLE}
+ 	sbatch --output ./out/03.vQC/${SAMPLE}_03.out --error ./err/03.vQC/${SAMPLE}_03.err --job-name vQC_${SAMPLE} 03.virus_enrichment_index.sh ${SAMPLE}
 done
