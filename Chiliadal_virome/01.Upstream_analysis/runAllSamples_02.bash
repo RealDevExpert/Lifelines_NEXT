13aac0df7c657e20127392509b1667528db5c14b
RealDevExpert <georgedotjmartinez@outlook.com>
2023-11-24 12:37:56 +0100

    Update runAllSamples_02.bash
    Changed the .out and .err location

--- Chiliadal_virome/01.Upstream_analysis/runAllSamples_02.bash
+++ Chiliadal_virome/01.Upstream_analysis/runAllSamples_02.bash
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 for SAMPLE in $@; do
- 	sbatch --output ./out/${SAMPLE}_02.out --error ./err/${SAMPLE}_02.err --job-name RAs_${SAMPLE} 02.sc_assembly.sh ${SAMPLE}
+ 	# Running the regular assembly script:
+	sbatch --output ./out/02.rAs/${SAMPLE}_02.out --error ./err/02.rAs/${SAMPLE}_02.err --job-name RAs_${SAMPLE} 02.sc_assembly.sh ${SAMPLE}
+	# Running the time-extended assembly script:
+	#sbatch --output ./out/${SAMPLE}_02.out --error ./err/${SAMPLE}_02.err --job-name RAs_${SAMPLE} 02a.sc_assembly.sh ${SAMPLE}
 done
