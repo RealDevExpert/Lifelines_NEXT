e9f3584972b61b30c29f93e432ddf378b7801472
RealDevExpert <georgedotjmartinez@outlook.com>
2024-03-05 14:55:04 +0100

    Update table_of_origin.R
    Changed the code organization

--- Chiliadal_virome/01.Upstream_analysis/table_of_origin.R
+++ Chiliadal_virome/01.Upstream_analysis/table_of_origin.R
@@ -1,17 +1,44 @@
+##########################################
+# Combining per sample table of origin  
+# and converting them from long to wide 
+# format
+##########################################
+args = commandArgs(trailingOnly=TRUE)
+##############################
+# Loading libraries
+##############################
 library(reshape2)
 library(dplyr)
 
-temp = list.files(path="../VIR_DB/table_of_origin",pattern="*_table_of_origin",full.names=T)
+##############################
+# Functions
+##############################
+
+##############################
+# Input data
+##############################
+
+##############################
+# ANALYSIS
+##############################
+temp = list.files(path="../VIR_DB/table_of_origin", pattern="*_table_of_origin", full.names=T)
 
 myfiles = lapply(temp, read.table)
 
+# coverting table of origin from long to wide format
 import_data=lapply(myfiles, function(x) {
                    out <- list(dcast(x, V1~V2))
                    return(out)
                    })
-table_of_origin_redundant<-bind_rows(import_data)
-table_of_origin_redundant[-1] <- as.integer(table_of_origin_redundant[-1] != 0) 
 
+# concatenating per sample tables of origin
+table_of_origin_redundant<-bind_rows(import_data)
+
+# converting tool names and NAs to boolean 
+table_of_origin_redundant[-1] <- as.integer(table_of_origin_redundant[-1] != 0) 
 table_of_origin_redundant[is.na(table_of_origin_redundant)] <- 0
 
+##############################
+# OUTPUT
+##############################
 write.table(table_of_origin_redundant, '../VIR_DB/table_of_origin/table_of_origin', sep='\t', quote=F, row.names=F)
