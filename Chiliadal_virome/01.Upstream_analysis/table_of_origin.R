6e35bd2a1d9ba2c2bfb780d8145e91224d7c313b
RealDevExpert <georgedotjmartinez@outlook.com>
2024-03-05 14:56:47 +0100

    Update table_of_origin.R
    Changed the input

--- Chiliadal_virome/01.Upstream_analysis/table_of_origin.R
+++ Chiliadal_virome/01.Upstream_analysis/table_of_origin.R
@@ -21,7 +21,7 @@ library(dplyr)
 ##############################
 # ANALYSIS
 ##############################
-temp = list.files(path="../VIR_DB/table_of_origin", pattern="*_table_of_origin", full.names=T)
+temp = list.files(path=args[1], pattern=args[2], full.names=T)
 
 myfiles = lapply(temp, read.table)
 
@@ -41,4 +41,4 @@ table_of_origin_redundant[is.na(table_of_origin_redundant)] <- 0
 ##############################
 # OUTPUT
 ##############################
-write.table(table_of_origin_redundant, '../VIR_DB/table_of_origin/table_of_origin', sep='\t', quote=F, row.names=F)
+write.table(table_of_origin_redundant, paste0(args[1], 'table_of_origin'), sep='\t', quote=F, row.names=F)
