641418b01c60062202dac2a108e077a5b997315d
RealDevExpert <georgedotjmartinez@outlook.com>
2024-03-06 22:38:03 +0100

    Update combine_coverage.R
    Changed read.table to read_delim from `readr` to speed up the process

--- Chiliadal_virome/01.Upstream_analysis/combine_coverage.R
+++ Chiliadal_virome/01.Upstream_analysis/combine_coverage.R
@@ -7,7 +7,7 @@ args = commandArgs(trailingOnly=TRUE)
 # Loading libraries
 ##############################
 library(tidyverse)
-
+library(readr)
 ##############################
 # Functions
 ##############################
@@ -20,8 +20,8 @@ library(tidyverse)
 # ANALYSIS
 ##############################
 # Selection of columns according to the bedtools coverage standard ouput:
-# V1: Contig names; V4: number of mapped reads; V7: breadth of coverage  
-columns_to_keep <- c("V1", "V4", "V7")
+# X1: Contig names; X4: number of mapped reads; X7: breadth of coverage  
+columns_to_keep <- c("X1", "X4", "X7")
 
 temp = list.files(path=args[1], pattern=args[2], full.names=T)
 
@@ -34,8 +34,9 @@ temp = list.files(path=args[1], pattern=args[2], full.names=T)
 myfiles = lapply(temp,
                  FUN = function(x){
                    print(gsub('\\..*', '', basename(x)))
-                   df <- read.table(x, sep='\t', header=F) 
-                   df <- df[,columns_to_keep]
+                   tbl <- read_delim(x, col_names = F)
+		   df <- as.data.frame(tbl)
+		   df <- df[,columns_to_keep]
                    return(df) 
                  })
 
@@ -51,16 +52,16 @@ for_count <- lapply(names(myfiles), function(list_name) {
   # Extract the dataframe
   df <- myfiles[[list_name]]
   
-  # Select V1 and V4 & rename V4 according to the file name
+  # Select X1 and X4 & rename X4 according to the file name
   df_selected <- df %>% 
-    select(V1, V4) %>%
-    rename(!!list_name := V4)
+    select(X1, X4) %>%
+    rename(!!list_name := X4)
   
   return(df_selected)
 })
 
 # Merge all selected and renamed dataframes into a single count table
-count_table <- for_count %>% reduce(full_join, by='V1')
+count_table <- for_count %>% reduce(full_join, by='X1')
 
 # Select and rename columns related to breadth of coverage
 for_coverage <- lapply(names(myfiles), function(list_name) {
@@ -68,16 +69,16 @@ for_coverage <- lapply(names(myfiles), function(list_name) {
   # Extract the dataframe
   df <- myfiles[[list_name]]
   
-  # Select V1 and V7 & rename V7 according to the file name
+  # Select X1 and X7 & rename X7 according to the file name
   df_selected <- df %>% 
-    select(V1, V7) %>%
-    rename(!!list_name := V7) 
+    select(X1, X7) %>%
+    rename(!!list_name := X7) 
   
   return(df_selected)
 })
 
 # Merge all selected and renamed dataframes into a single breadth of coverage talbe
-coverage_table <- for_coverage %>% reduce(full_join, by='V1')
+coverage_table <- for_coverage %>% reduce(full_join, by='X1')
 
 write.table(coverage_table, paste0(args[1], "coverage_table.txt"),  sep='\t', row.names=F, quote=F)
 write.table(count_table, paste0(args[1], "count_table.txt"), sep='\t', row.names=F, quote=F)
