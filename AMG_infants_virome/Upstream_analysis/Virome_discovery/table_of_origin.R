dbe2f6379b5e91f58adfecb4f2161014a2f3f7d5
RealDevExpert <georgedotjmartinez@outlook.com>
2024-05-28 15:19:25 +0200

    Add files via upload

---
+++ AMG_infants_virome/Upstream_analysis/Virome_discovery/table_of_origin.R
@@ -0,0 +1,61 @@
+##########################################
+# Combining per sample table of origin  
+# and converting them from long to wide 
+# format
+
+##########################################
+.libPaths("/home1/p309176/R_libraries")
+args = commandArgs(trailingOnly=TRUE)
+##############################
+# Loading libraries
+##############################
+library(reshape2)
+library(dplyr)
+
+##############################
+# Functions
+##############################
+
+##############################
+# Input data
+##############################
+
+##############################
+# ANALYSIS
+##############################
+temp = list.files(path=args[1], pattern=args[2], full.names=T)
+
+# Check if the list of files is not empty
+if (length(temp) == 0) {
+	  cat("No files matching the specified pattern found. Exiting script.\n")
+  q("no")
+}
+
+# Filter out empty files
+temp <- temp[sapply(temp, function(x) file.info(x)$size > 0)]
+
+# Check if there are any non-empty files
+if (length(temp) == 0) {
+	  cat("All files are empty. Exiting script.\n")
+  q("no")
+}
+
+myfiles = lapply(temp, read.table)
+
+# coverting table of origin from long to wide format
+import_data=lapply(myfiles, function(x) {
+                   out <- list(dcast(x, V1~V2))
+                   return(out)
+                   })
+
+# concatenating per sample tables of origin
+table_of_origin_redundant<-bind_rows(import_data)
+
+# converting tool names and NAs to boolean 
+table_of_origin_redundant[-1] <- as.integer(table_of_origin_redundant[-1] != 0) 
+table_of_origin_redundant[is.na(table_of_origin_redundant)] <- 0
+
+##############################
+# OUTPUT
+##############################
+write.table(table_of_origin_redundant, paste0(args[1], 'table_of_origin'), sep='\t', quote=F, row.names=F)
